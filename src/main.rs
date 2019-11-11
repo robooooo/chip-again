9513c6da7f6b4b791904dc62413d90179c3765af
HTune <atune313@gmail.com>
2019-11-11 00:06:04 +0000

    Playing around with StructOpt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+use std::{
+    path::PathBuf,
+};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "chip-again", about = "Yet another chip-8 emulator (because YAC8 was taken)")]
+struct Opt {
+    #[structopt(parse(from_os_str), name = "rom", help = "Path to a chip8 compatible ROM file.")]
+    input: PathBuf,
+}
+
 fn main() {
-    
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
